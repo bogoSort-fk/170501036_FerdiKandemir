b6e754c4a7db4a96682c4fb9be7be89f62c73472
Ferdi Kandemir <ferdi.kandemir@siemens.com>
2022-04-12 20:22:28 +0300

    First version with classes.

---
+++ Rechtsbuero_Desktop/Rechtsbuero_Desktop/Admin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechtsbuero_Desktop
+{
+    internal class Admin
+    {
+        private string userName, password;
+
+        public Admin(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public string UserName { get => userName; set => userName = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
